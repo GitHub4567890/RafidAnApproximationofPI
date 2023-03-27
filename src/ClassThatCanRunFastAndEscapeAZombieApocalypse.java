f8955ac18e81d3ec1ee9bc510e677b08229c083e
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2023-03-26 23:30:57 -0400

    1

--- src/ClassThatCanRunFastAndEscapeAZombieApocalypse.java
+++ src/ClassThatCanRunFastAndEscapeAZombieApocalypse.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ClassThatCanRunFastAndEscapeAZombieApocalypse {
+public class ClassThatCanRunFastAndEscapeAZombieApocalypse {
+    public static void main(String[] args) {
+        int count = 0;
+        int totalInside = 0;
+        while (count <= 1000000) {
+            double pointX = Math.random();
+            double pointY = Math.random();
+            if (Math.sqrt((Math.pow(pointX, 2)) + (Math.pow(pointY, 2))) <= 1) {
+                totalInside++;
+            }
+            count++;
+        }
+        double result = ((double) totalInside/count) * 4;
+        System.out.println("Result: " + result);
+    }
 }
